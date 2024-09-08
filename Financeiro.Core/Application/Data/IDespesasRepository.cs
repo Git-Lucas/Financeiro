7c4412c283c3354a258a4b1aedaedc9240835015
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-09-08 18:09:07 -0300

    feat: Criado projeto que representa o 'core' do sistema financeiro, com as definições das 'models' e dos 'useCases'

---
+++ Financeiro.Core/Application/Data/IDespesasRepository.cs
@@ -0,0 +1,7 @@
+﻿using Financeiro.Core.Domain.Despesas.Models;
+
+namespace Financeiro.Core.Application.Data;
+public interface IDespesasRepository
+{
+    Task<IEnumerable<Despesa>> RecuperarDespesasPorPeriodoAsync(int mes, int ano);
+}
