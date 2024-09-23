00d8ad597e4f105ad73d004badd0f55ac134e616
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-09-23 13:26:43 -0300

    refactor: Removido DomainService 'RecuperarDespesasPorPeriodo', que não continha regra adicional alguma, e utilizado na camada de 'Application' método do Repository

--- Financeiro.Application/UseCases/R-2020/EnviarR2020.cs
+++ Financeiro.Application/UseCases/R-2020/EnviarR2020.cs
@@ -1,6 +1,6 @@
 ﻿using Financeiro.Application.DependencyInjection;
 using Financeiro.Application.UseCases.DespesasPorPeriodo.DTOs;
-using Financeiro.Domain.Despesas.DomainServices;
+using Financeiro.Domain.Data.Interfaces;
 using Financeiro.Domain.Despesas.Models;
 using Financeiro.Domain.EfdReinf.DTOs;
 using Financeiro.Domain.EfdReinf.Interfaces;
@@ -8,12 +8,12 @@ using Financeiro.Domain.EfdReinf.Interfaces;
 namespace Financeiro.Application.UseCases.R_2020;
 public class EnviarR2020(IEfdReinf efdReinf)
 {
-    private readonly RecuperarDespesasPorPeriodo _domainService = new(ContainerDependencyInjection.DespesasRepository);
+    private readonly IDespesasRepository _despesasRepository = ContainerDependencyInjection.DespesasRepository;
     private readonly IEfdReinf _efdReinf = efdReinf;
 
     public async Task<EnviarR2020Resposta> ExecuteAsync(EnviarR2020Requisicao requisicao)
     {
-        IEnumerable<Despesa> despesasFiltradas = await _domainService.ExecuteAsync(requisicao.Mes, requisicao.Ano);
+        IEnumerable<Despesa> despesasFiltradas = await _despesasRepository.RecuperarDespesasPorPeriodoVencimentoAsync(requisicao.Mes, requisicao.Ano);
 
         EnviarR2020Resposta efdReinfResposta = await _efdReinf.EnviarR2020Async(despesasFiltradas);
 
