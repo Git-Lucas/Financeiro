9ce74da67fbf9b7d65dbeaf988e3450d5f7e3d69
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-10-17 18:53:44 -0300

    feat: Adicionado DataAnnotation de validação para campo de 'ValorTotal' de 'Despesa'

--- Financeiro.Domain/Despesas/Models/Despesa.cs
+++ Financeiro.Domain/Despesas/Models/Despesa.cs
@@ -1,4 +1,6 @@
 ﻿using Financeiro.Domain.ParceirosComerciais.Models;
+using Financeiro.Domain.Util;
+using System.ComponentModel.DataAnnotations;
 
 namespace Financeiro.Domain.Despesas.Models;
 public class Despesa
@@ -7,6 +9,8 @@ public class Despesa
     public ParceiroComercial Beneficiario { get; private set; }
     public DateTime DataEmissao { get; private set; }
     public DateTime DataVencimento { get; private set; }
+
+    [Range(0, double.MaxValue, ErrorMessage = "O {0} deve ser maior que {1}.")]
     public decimal ValorTotal { get; private set; }
 
     public Despesa(ParceiroComercial beneficiario, DateTime dataEmissao, DateTime dataVencimento, decimal valorTotal)
@@ -15,7 +19,12 @@ public class Despesa
         DataEmissao = dataEmissao;
         DataVencimento = dataVencimento;
         ValorTotal = valorTotal;
+
+        ModelsValidator.Execute(this);
     }
 
-    public Despesa() { }
+    public Despesa()
+    {
+        ModelsValidator.Execute(this);
+    }
 }
